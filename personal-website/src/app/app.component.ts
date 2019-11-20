18ea3e5935dce6948bb34eb4f5ce63395301ed54
mthaker1 <thakermanas5@gmail.com>
2019-11-19 23:52:20 -0500

    Build changes

--- personal-website/src/app/app.component.ts
+++ personal-website/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { slider, transformer, fader, stepper } from "./route-animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
-  animations: [slider]
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   title = "personal-website";
